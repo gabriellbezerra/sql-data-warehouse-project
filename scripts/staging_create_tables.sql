09a18be4705cdf9dc818ffc87be45a74633a4ca4
gabriellbezerra <ackermanngabriell@gmail.com>
2026-01-08 12:02:21 -0300

    fix: increase the varchar length.

--- scripts/staging_create_tables.sql
+++ scripts/staging_create_tables.sql
@@ -13,30 +13,30 @@ WARNING:
 DROP TABLE IF EXISTS staging.crm_customer_info;
 CREATE TABLE staging.crm_customer_info(
 	customer_id integer,
-	customer_key character varying(20),
-	customer_first_name character varying(15),
-	customer_last_name character varying(15),
-	customer_marital_status character varying(1),
-	customer_gender character varying(1),
+	customer_key character varying(50),
+	customer_first_name character varying(50),
+	customer_last_name character varying(50),
+	customer_marital_status character varying(50),
+	customer_gender character varying(50),
 	customer_create_date date
 );
 
 DROP TABLE IF EXISTS staging.crm_product_info;
 CREATE TABLE staging.crm_product_info(
     product_id integer,
-    product_key character varying(20),
+    product_key character varying(50),
     product_name character varying(50),
     product_cost integer,
-    product_line character varying(5),
+    product_line character varying(50),
     product_start_date date,
     product_end_date date
 );
 
 DROP TABLE IF EXISTS staging.crm_sales_details;
 CREATE TABLE staging.crm_sales_details(
-    sales_order_number character varying(20),
-    sales_product_key character varying(20),
-    sales_customer_id character varying(20),
+    sales_order_number character varying(50),
+    sales_product_key character varying(50),
+    sales_customer_id character varying(50),
     sales_order_date integer,
     sales_ship_date integer,
     sales_due_date integer,
@@ -49,19 +49,19 @@ DROP TABLE IF EXISTS staging.erp_cust_az12;
 CREATE TABLE staging.erp_cust_az12(
     c_id character varying(50),
     birth_date date,
-    gender character varying(1)
+    gender character varying(50)
 );
 
 DROP TABLE IF EXISTS staging.erp_loc_a101;
 CREATE TABLE staging.erp_loc_a101(
-    c_id character varying(12),
-    country character varying(10)
+    c_id character varying(50),
+    country character varying(50)
 );
 
 DROP TABLE IF EXISTS staging.erp_px_cat_g1v2;
 CREATE TABLE staging.erp_px_cat_g1v2(
-    id character varying(10),
-    category character varying(20),
-    sub_category character varying(20),
-    maintenance character varying(3)
+    id character varying(50),
+    category character varying(50),
+    sub_category character varying(50),
+    maintenance character varying(50)
 );
