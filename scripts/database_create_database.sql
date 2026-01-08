365911f5aafc1149bedea5a5c956bfce9ecf59c6
gabriellbezerra <ackermanngabriell@gmail.com>
2026-01-08 10:41:59 -0300

    feat: add the database creation script.

---
+++ scripts/database_create_database.sql
@@ -0,0 +1,13 @@
+/*
+=============================================================
+Create the database
+=============================================================
+Script Purpose:
+    This script creates the database.
+	
+WARNING:
+    Running this script will drop all database and recreate it. Be sure to backup everything first.
+*/
+
+DROP database IF EXISTS Datawarehouse;
+CREATE DATABASE Datawarehouse;
